17df4f32998694d0dc7e36d9a701810b4def979d
felix <felixpetre@gmail.com>
2023-12-03 14:04:38 +0100

    changed config

--- src/main/java/org/matsim/project/RunMatsimApplication.java
+++ src/main/java/org/matsim/project/RunMatsimApplication.java
@@ -71,4 +71,13 @@ public class RunMatsimApplication extends MATSimApplication {
 
 		// ---
 	}
+	controler.addOverridingModule(new AbstractModule() {
+		@Override
+		public void install() {
+
+			addTravelTimeBinding(TransportMode.ride).to(networkTravelTime());
+			addTravelDisutilityFactoryBinding(TransportMode.ride).to(carTravelDisutilityFactoryKey());
+
+		}
+	});
 }
